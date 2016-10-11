c5aa1c0a7ced1d317bb7b55ad5d67ef591729af2
Tianshi Cheng <chengts95@163.com>
2016-10-11 10:15:25 +0800

    update 1

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var maps = require('./routes/map');
+var login = require('./routes/login');
 var app = express();
 
 // view engine setup
@@ -24,7 +25,7 @@ app.use('/static',express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/login', login);
 app.use('/map', maps);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
